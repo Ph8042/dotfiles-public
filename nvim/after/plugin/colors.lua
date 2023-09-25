c99022dd4e183025a3c760c5cfc752a4826e9e4f
Phh8042 <109532432+Ph8042@users.noreply.github.com>
2023-09-25 22:38:05 +0200

    neovim

---
+++ nvim/after/plugin/colors.lua
@@ -0,0 +1,18 @@
+-- Lua
+
+--vim.cmd[[colorscheme tokyonight-night]]
+--vim.cmd[[colorscheme tokyonight-storm]]
+--vim.cmd[[colorscheme tokyonight-day]]
+--vim.cmd[[colorscheme tokyonight-moon]]
+
+Disable_background = false
+
+function ColorMyPencils()
+    vim.cmd[[colorscheme tokyonight-storm]]
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+ColorMyPencils()
---
+++ nvim/init.lua
@@ -0,0 +1,1 @@
+require("phh8042")
