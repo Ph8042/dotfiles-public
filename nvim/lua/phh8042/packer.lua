e6cdcce753aac5074ed14638a9b00d859fed828b
Phh8042 <109532432+Ph8042@users.noreply.github.com>
2023-10-04 22:49:45 +0200

    Get rid of nvim-tree

--- nvim/lua/phh8042/packer.lua
+++ nvim/lua/phh8042/packer.lua
@@ -60,13 +60,6 @@ return require('packer').startup(function(use)
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
 
-    use {
-        'nvim-tree/nvim-tree.lua',
-        requires = {
-            'nvim-tree/nvim-web-devicons', -- optional
-        },
-    }
-
 
 
 end)
